332db0a8a2f69df8fbc963b7c1cf4522d67b4265
yeongjun <dudwns650395@gmail.com>
2022-11-22 15:50:36 +0900

    Add #1 - Add Tomcat & Configuration
    톰캣을 설치하고, 웹을 띄울 수 있는 설정 추가.

--- src/main/java/com/yeong/config/Config.java
+++ src/main/java/com/yeong/config/Config.java
@@ -2,7 +2,9 @@ package com.yeong.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @ComponentScan("com.yeong")
 @Configuration
 public class Config {
