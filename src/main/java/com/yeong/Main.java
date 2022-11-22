82cfe31f1f1a5d58aad002bc4fa54fc42c19db93
yeongjun <dudwns650395@gmail.com>
2022-11-22 15:15:40 +0900

    Initialize #1 - Java OOP
    Java OOP 프로젝트에 스프링 적용하기
    
    * https://github.com/dudwns6503/java-oop

---
+++ src/main/java/com/yeong/Main.java
@@ -0,0 +1,14 @@
+package com.yeong;
+
+import com.yeong.logic.JavaSort;
+import com.yeong.logic.Sort;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Sort<String> sort = new JavaSort<>();
+
+        System.out.println("[result] " + sort.sort(Arrays.asList(args)));
+    }
+}
